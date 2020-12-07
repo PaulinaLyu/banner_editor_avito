cca50f383f6cf6bfcb6a7562a022d6426a7100ca
PaulinaLyu <goodbad8002@gmail.com>
2020-12-08 01:25:24 +0300

    gitHub Page

--- src/assets/js/components/btnPNG.js
+++
@@ -1,8 +0,0 @@
-btnPNG.addEventListener('click', () => {
-    html2canvas(bannerPreview)
-        .then(canvas => {
-            canvas.toBlob(function(blob) {
-                saveAs(blob, "banner.png");
-            });
-        });
-});
--- src/assets/js/components/buildJsonFormData.js
+++
@@ -1,7 +0,0 @@
-function buildJsonFormData(form) {
-    const jsonFormData = { };
-    for(const pair of new FormData(form)) {
-        jsonFormData[pair[0]] = pair[1]
-    }
-    return jsonFormData
-};
